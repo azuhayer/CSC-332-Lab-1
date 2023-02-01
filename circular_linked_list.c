88476b9389bc2694ed83708037423d6d6afeb964
Zuhayer Alvi <azuhayer@gmail.com>
2023-02-01 15:18:07 -0500

    Update circular_linked_lis.c

--- circular_linked_list.c
+++ circular_linked_list.c
@@ -1,1 +1,6 @@
 /* Implement a Circular Link list using C. The link list should have traverse, insert, delete, search and sort operations. */
+
+struct node {
+    int data;
+    struct node* next;
+};
